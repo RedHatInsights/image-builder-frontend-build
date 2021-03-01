754ace36953023c165278b7980b4ddb92a21502d
Travis CI User <travis@example.org>
2021-03-01 16:04:54 +0000

    CreateImageWizard: update review page
    The review page now uses more patternfly components and has been split
    up a little so it is easier to add review fields for additional target
    environments.

--- js/1.4246adee2fbc8f31a3a1.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{635:function(n,o,e){"use strict";e.d(o,"a",(function(){return t}));var p=e(370);var t={updateCompose:function(n){return{type:p.a.UPDATE_COMPOSE,compose:n}}}}}]);
-//# sourceMappingURL=1.4246adee2fbc8f31a3a1.js.map

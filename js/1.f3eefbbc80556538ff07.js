5cf26bb4e04355c7f0f161cfad96f59427f8257c
Travis CI User <travis@example.org>
2021-03-08 10:50:58 +0000

    azure: fix the oauth link
    Multi-tenant access needs the v2.0 route that requires:
    - scope
    - response_type
    
    I also also added response_mode=form_post so the token isn't shown in
    the address bar.

--- js/1.f3eefbbc80556538ff07.js
+++ js/1.f3eefbbc80556538ff07.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[1],{637:function(n,o,e){"use strict";e.d(o,"a",(function(){return t}));var p=e(370);var t={updateCompose:function(n){return{type:p.a.UPDATE_COMPOSE,compose:n}}}}}]);
-//# sourceMappingURL=1.776410eff3e6eff5b130.js.map
+//# sourceMappingURL=1.f3eefbbc80556538ff07.js.map

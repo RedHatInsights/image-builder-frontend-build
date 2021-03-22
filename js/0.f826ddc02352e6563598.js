57c0b0f54b61f842edeb63ff44b3b7fe2318b340
Travis CI User <travis@example.org>
2021-03-22 11:07:15 +0000

    CreateImageWizard: single upload_request
    Change imageRequest to accept a single uploadRequest instead of
    an array of uploadRequests.

--- js/0.f826ddc02352e6563598.js
+++ js/0.f826ddc02352e6563598.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[0],{686:function(n,o,e){"use strict";e.d(o,"a",(function(){return t}));var p=e(397);var t={updateCompose:function(n){return{type:p.a.UPDATE_COMPOSE,compose:n}}}}}]);
-//# sourceMappingURL=0.f1db91dabd650ce2acd0.js.map
+//# sourceMappingURL=0.f826ddc02352e6563598.js.map

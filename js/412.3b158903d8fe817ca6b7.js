d9f48b77a37bb2c353c965fb392cfdff150a7d53
Travis CI User <travis@example.org>
2021-05-12 15:26:04 +0000

    src: Have separate AppEntry points for dev and prod
    The production AppEntry shouldn't pass any middlewares to the store.

--- js/412.3b158903d8fe817ca6b7.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,r,t)=>{"use strict";t.r(r),t.d(r,{default:()=>p});var n=t(31154),a=t.n(n),l=t(45697),o=t.n(l),u=t(16530),s=t(88931),c=t(11916),i=t(66986),g=t(35240),m=function(e){var r=e.logger;return a().createElement(s.Provider,{store:(0,c.S1)({},r).getStore()},a().createElement(u.BrowserRouter,{basename:(0,g.eb)(window.location.pathname)},a().createElement(i.Z,null)))};m.propTypes={logger:o().any};const p=m}}]);
-//# sourceMappingURL=../sourcemaps/412.d89986c46de21607f453.js.map

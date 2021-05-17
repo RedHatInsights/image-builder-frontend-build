913d47329e57f0dcb72bc64e3e97f8b0d0eb65a1
Travis CI User <travis@example.org>
2021-05-17 17:40:31 +0000

    ImagesTable: add link to launch uploaded aws image
    The images list now contains a link to the ec2 launch wizard for a
    successfully uploaded aws image.

--- js/156.a8194414d40172442eb9.js
+++ js/156.a8194414d40172442eb9.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[156],{96156:(e,u,i)=>{"use strict";function r(e,u,i){return u in e?Object.defineProperty(e,u,{value:i,enumerable:!0,configurable:!0,writable:!0}):e[u]=i,e}i.d(u,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/156.fc2057906d3eb14d28e8.js.map
+//# sourceMappingURL=../sourcemaps/156.dc671f03ad199312a806.js.map

5b03ea54de078c01b134cd0d053f3bd003c04ede
Travis CI User <travis@example.org>
2021-05-20 12:23:11 +0000

    ImagesTable: display date created
    The date an image was created at should be displayed in the format:
    Month Day, Year. i.e. Apr 28, 2021.

--- js/665.fc1b55b58245c4ff1261.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[665],{14665:(e,t,u)=>{"use strict";function r(e,t){return(r=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}u.d(t,{Z:()=>r})}}]);
--- js/910.0470647a55f7b57ad1ae.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[910],{75347:()=>{},36648:()=>{},31777:()=>{},34017:()=>{},60056:()=>{},64604:()=>{}}]);

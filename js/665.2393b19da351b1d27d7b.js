44a46ea2ab273b9e9bae92e2d9440df1a7f3e7b7
Travis CI User <travis@example.org>
2021-05-26 15:05:17 +0000

    CreateImageWizard: improve packages step
    Update the text description and replace "options" with "packages". Also,
    add styling to increase the size of the available and chosen packages
    list.

--- js/665.2393b19da351b1d27d7b.js
+++ js/665.2393b19da351b1d27d7b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[665],{14665:(e,t,u)=>{"use strict";function r(e,t){return(r=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}u.d(t,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/665.4b3667f310cb95743889.js.map
+//# sourceMappingURL=../sourcemaps/665.28d2731a545c014a3ae1.js.map

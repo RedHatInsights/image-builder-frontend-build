1582188d589467ae1da569e335379d446b34efd9
Travis CI User <travis@example.org>
2021-06-02 13:47:22 +0000

    ImagesTable: add gcp info
    When a gcp image is successfully uploaded the user needs to know the
    project id, image name, and account the image is shared with. These are
    now displayed in a popover accessible from the status section of the
    images table.

--- js/665.2393b19da351b1d27d7b.js
+++ js/665.2393b19da351b1d27d7b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[665],{14665:(e,t,u)=>{"use strict";function r(e,t){return(r=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}u.d(t,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/665.0dccc6442b2dafd0a682.js.map
+//# sourceMappingURL=../sourcemaps/665.dfe5871efe151315ac5e.js.map

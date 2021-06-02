290fb48b20b338b132da90ca52ff069e970489f1
Travis CI User <travis@example.org>
2021-06-02 15:59:19 +0000

    store: Insert new composes on top of the table
    Fixes #161

--- js/446.dfbf2fc2d8bd7b988a73.js
+++ js/446.dfbf2fc2d8bd7b988a73.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[446],{18446:(e,u,r)=>{var a=r(90939);e.exports=function(e,u){return a(e,u)}}}]);
-//# sourceMappingURL=../sourcemaps/446.f5a337780fd863bfa9dd.js.map
+//# sourceMappingURL=../sourcemaps/446.78db574de59bb6498ee1.js.map

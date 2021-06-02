ffffc564b29fe8d06e58a310e7077456a8dadc5c
Travis CI User <travis@example.org>
2021-06-02 15:59:11 +0000

    store: Insert new composes on top of the table
    Fixes #161

--- js/665.2393b19da351b1d27d7b.js
+++ js/665.2393b19da351b1d27d7b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[665],{14665:(e,t,u)=>{"use strict";function r(e,t){return(r=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}u.d(t,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/665.f5a337780fd863bfa9dd.js.map
+//# sourceMappingURL=../sourcemaps/665.78db574de59bb6498ee1.js.map
--- js/843.f3d03ea4597e18adac91.js
+++ js/843.f3d03ea4597e18adac91.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[843],{28368:(e,i,o)=>{var n=o(90939);e.exports=function(e,i,o){var r=(o="function"==typeof o?o:void 0)?o(e,i):void 0;return void 0===r?n(e,i,void 0,o):!!r}},30236:(e,i,o)=>{var n=o(42980),r=o(21463)((function(e,i,o,r){n(e,i,o,r)}));e.exports=r}}]);
-//# sourceMappingURL=../sourcemaps/843.f5a337780fd863bfa9dd.js.map
+//# sourceMappingURL=../sourcemaps/843.78db574de59bb6498ee1.js.map

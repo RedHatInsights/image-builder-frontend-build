0988bf108a1a49c6b168e5ced550e0e409314700
Travis CI User <travis@example.org>
2021-06-02 13:47:39 +0000

    CreateImageWizard: alphabetize upload destinations
    The upload destinations now display in alphabetical order.

--- js/446.dfbf2fc2d8bd7b988a73.js
+++ js/446.dfbf2fc2d8bd7b988a73.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[446],{18446:(e,u,r)=>{var a=r(90939);e.exports=function(e,u){return a(e,u)}}}]);
-//# sourceMappingURL=../sourcemaps/446.dfe5871efe151315ac5e.js.map
+//# sourceMappingURL=../sourcemaps/446.5d9b1b014102afa13c30.js.map

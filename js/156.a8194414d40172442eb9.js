2ccf3b244655d4c2c29bfd9036522ca1daab8002
Travis CI User <travis@example.org>
2021-06-10 08:33:41 +0000

    package.json: Update @patternfly/react-table, @patternfly/react-core, @patternfly/patternfly
    Closes #231

--- js/156.a8194414d40172442eb9.js
+++ js/156.a8194414d40172442eb9.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[156],{96156:(e,u,i)=>{"use strict";function r(e,u,i){return u in e?Object.defineProperty(e,u,{value:i,enumerable:!0,configurable:!0,writable:!0}):e[u]=i,e}i.d(u,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/156.6eb38640e68b47b14742.js.map
+//# sourceMappingURL=../sourcemaps/156.9e8e910ec47b0b847e8c.js.map

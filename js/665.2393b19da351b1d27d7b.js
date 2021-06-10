c85513b43e9b7d5e69cb125c5b9bfd1e0e25d8e5
Travis CI User <travis@example.org>
2021-06-10 08:12:50 +0000

    package.json: Update @redhat-cloud-services/frontend-components
    Closes #228

--- js/665.2393b19da351b1d27d7b.js
+++ js/665.2393b19da351b1d27d7b.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[665],{14665:(e,t,u)=>{"use strict";function r(e,t){return(r=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}u.d(t,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/665.f4f282544807acd01ade.js.map
+//# sourceMappingURL=../sourcemaps/665.6eb38640e68b47b14742.js.map

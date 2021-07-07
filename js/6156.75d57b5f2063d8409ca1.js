20ac2bf16a4c8d54777ea433298c087f963e318a
Travis CI User <travis@example.org>
2021-07-07 14:52:59 +0000

    package.json: Update @data-driven-forms/react-form-renderer
    Closes #250

--- js/6156.75d57b5f2063d8409ca1.js
+++ js/6156.75d57b5f2063d8409ca1.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[6156],{96156:(e,u,i)=>{"use strict";function r(e,u,i){return u in e?Object.defineProperty(e,u,{value:i,enumerable:!0,configurable:!0,writable:!0}):e[u]=i,e}i.d(u,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/6156.0a1688e58521e724afeb.js.map
+//# sourceMappingURL=../sourcemaps/6156.c147caa8c5c10be8bba5.js.map

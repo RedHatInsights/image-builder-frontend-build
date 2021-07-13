a6b00419dda12078357abb0710002aa05a78e956
Travis CI User <travis@example.org>
2021-07-13 08:13:05 +0000

    CreateImageWizard: Make instructions on Review page normal font size
    Closes #253

--- js/4665.edea34ee0787f0e30501.js
+++ js/4665.edea34ee0787f0e30501.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[4665],{14665:(e,t,u)=>{"use strict";function r(e,t){return(r=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}u.d(t,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/4665.f5bdb39d1df8076f98a8.js.map
+//# sourceMappingURL=../sourcemaps/4665.39faec1bae02fce5b1f8.js.map
--- js/9665.83ffb58d4570abef92fe.js
+++ js/9665.83ffb58d4570abef92fe.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[9665],{28368:(e,i,o)=>{var n=o(90939);e.exports=function(e,i,o){var r=(o="function"==typeof o?o:void 0)?o(e,i):void 0;return void 0===r?n(e,i,void 0,o):!!r}},30236:(e,i,o)=>{var n=o(42980),r=o(21463)((function(e,i,o,r){n(e,i,o,r)}));e.exports=r}}]);
-//# sourceMappingURL=../sourcemaps/9665.f5bdb39d1df8076f98a8.js.map
+//# sourceMappingURL=../sourcemaps/9665.39faec1bae02fce5b1f8.js.map

2a0eae4148d6829059e7bb5dc0f2cf0a04b61222
Travis CI User <travis@example.org>
2021-07-13 08:14:34 +0000

    CreateImageWizard: Fix inconsistent casing Google products
    Closes #259

--- js/8446.b0474ca90ddac88d21ea.js
+++ js/8446.b0474ca90ddac88d21ea.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[8446],{18446:(e,u,r)=>{var a=r(90939);e.exports=function(e,u){return a(e,u)}}}]);
-//# sourceMappingURL=../sourcemaps/8446.39faec1bae02fce5b1f8.js.map
+//# sourceMappingURL=../sourcemaps/8446.1e1517249bdbbd6a75ab.js.map

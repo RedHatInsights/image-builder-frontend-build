35efc1ba710d04e5e547b98d73fcc36ddce561bb
Travis CI User <travis@example.org>
2021-07-20 15:52:34 +0000

    README: Update URL of UI

--- js/412.0489e9f916ef19a9a0d3.js
+++ js/412.0489e9f916ef19a9a0d3.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>s});var n=r(31154),a=r.n(n),l=r(16530),u=r(88931),o=r(11916),c=r(66986),i=r(35240);const s=function(){return a().createElement(u.Provider,{store:(0,o.S1)({}).getStore()},a().createElement(l.BrowserRouter,{basename:(0,i.eb)(window.location.pathname)},a().createElement(c.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.5edf87fb59cde5f17626.js.map
+//# sourceMappingURL=../sourcemaps/412.2b010d7ae0b2034bdfcd.js.map

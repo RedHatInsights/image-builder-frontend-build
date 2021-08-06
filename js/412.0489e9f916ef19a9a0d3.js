453ec74918f1867ab9c90677d8f252b247b65372
Travis CI User <travis@example.org>
2021-08-06 10:42:51 +0000

    package.json: Update redux
    Closes #280

--- js/412.0489e9f916ef19a9a0d3.js
+++ js/412.0489e9f916ef19a9a0d3.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>s});var n=r(31154),a=r.n(n),l=r(16530),u=r(88931),o=r(11916),c=r(66986),i=r(35240);const s=function(){return a().createElement(u.Provider,{store:(0,o.S1)({}).getStore()},a().createElement(l.BrowserRouter,{basename:(0,i.eb)(window.location.pathname)},a().createElement(c.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.e18c2045fc678d97d7f2.js.map
+//# sourceMappingURL=../sourcemaps/412.c4ce83ba0b30fb628f40.js.map
--- js/491.23a2c5d0cb39b2910dee.js
+++ js/491.23a2c5d0cb39b2910dee.js
@@ -1,2 +1,2 @@
 (self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{"use strict";n.r(t);var r=n(31154),a=n.n(r),l=n(46265),o=n.n(l),u=n(16530),c=n(88931),m=n(11916),i=n(66986),s=n(35240);const d=function(){return a().createElement(c.Provider,{store:(0,m.S1)({}).getStore()},a().createElement(u.BrowserRouter,{basename:(0,s.eb)(window.location.pathname)},a().createElement(i.Z,null)))};o().render(a().createElement(d,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.e18c2045fc678d97d7f2.js.map
+//# sourceMappingURL=../sourcemaps/491.c4ce83ba0b30fb628f40.js.map

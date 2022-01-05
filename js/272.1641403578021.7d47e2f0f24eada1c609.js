0e2819b8c85ee9f6b7ab9e75f8340160a7b65ab3
Travis CI User <travis@example.org>
2022-01-05 17:38:41 +0000

    build(deps-dev): bump @babel/preset-react from 7.16.0 to 7.16.7
    Bumps [@babel/preset-react](https://github.com/babel/babel/tree/HEAD/packages/babel-preset-react) from 7.16.0 to 7.16.7.
    - [Release notes](https://github.com/babel/babel/releases)
    - [Changelog](https://github.com/babel/babel/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/babel/babel/commits/v7.16.7/packages/babel-preset-react)
    
    ---
    updated-dependencies:
    - dependency-name: "@babel/preset-react"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/272.1641403578021.7d47e2f0f24eada1c609.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[272],{87462:(e,r,t)=>{function n(){return n=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var t=arguments[r];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e},n.apply(this,arguments)}t.d(r,{Z:()=>n})}}]);
-//# sourceMappingURL=../sourcemaps/272.eabf5734d1cd1ee02e31a69a8a93c5d0.js.map

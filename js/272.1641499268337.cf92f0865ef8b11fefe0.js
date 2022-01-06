27cd168ba7f44439a670372e064df563baa0da7d
Travis CI User <travis@example.org>
2022-01-06 20:20:45 +0000

    build(deps-dev): bump @babel/plugin-transform-runtime
    Bumps [@babel/plugin-transform-runtime](https://github.com/babel/babel/tree/HEAD/packages/babel-plugin-transform-runtime) from 7.16.4 to 7.16.7.
    - [Release notes](https://github.com/babel/babel/releases)
    - [Changelog](https://github.com/babel/babel/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/babel/babel/commits/v7.16.7/packages/babel-plugin-transform-runtime)
    
    ---
    updated-dependencies:
    - dependency-name: "@babel/plugin-transform-runtime"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/272.1641499268337.cf92f0865ef8b11fefe0.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[272],{87462:(e,r,t)=>{function n(){return n=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var t=arguments[r];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e},n.apply(this,arguments)}t.d(r,{Z:()=>n})}}]);
-//# sourceMappingURL=../sourcemaps/272.b33dc223f0d9c3366794903b63526666.js.map

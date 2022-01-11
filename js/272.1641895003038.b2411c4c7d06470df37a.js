93794c52f9d8d47cebf5d3b7cca776c7243cbe28
Travis CI User <travis@example.org>
2022-01-11 09:59:05 +0000

    CreateImageWizard: validate azure resource group
    Resource group names only allow alphanumeric characters, periods,
    underscores, hyphens, and parenthesis and cannot end in a period. A
    regex validation is now added to ensure this.
    
    Documented by azure here:
    https://docs.microsoft.com/en-us/rest/api/resources/resource-groups/create-or-update

---
+++ js/272.1641895003038.b2411c4c7d06470df37a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[272],{87462:(e,r,t)=>{function n(){return n=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var t=arguments[r];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e},n.apply(this,arguments)}t.d(r,{Z:()=>n})}}]);
+//# sourceMappingURL=../sourcemaps/272.363e055d0f1f209cd2c0ce2cae00091c.js.map

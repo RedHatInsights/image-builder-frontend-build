1d4ed4deea93a9feae59927f09c250237d2f3cee
Travis CI User <travis@example.org>
2022-01-13 15:23:31 +0000

    build(deps-dev): bump sass from 1.47.0 to 1.48.0
    Bumps [sass](https://github.com/sass/dart-sass) from 1.47.0 to 1.48.0.
    - [Release notes](https://github.com/sass/dart-sass/releases)
    - [Changelog](https://github.com/sass/dart-sass/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/sass/dart-sass/compare/1.47.0...1.48.0)
    
    ---
    updated-dependencies:
    - dependency-name: sass
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/272.1642078276657.f04640f540cc9088379d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[272],{87462:(e,r,t)=>{function n(){return n=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var t=arguments[r];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e},n.apply(this,arguments)}t.d(r,{Z:()=>n})}}]);
-//# sourceMappingURL=../sourcemaps/272.0674adb902a96bf4f691259aaa8f2ae8.js.map

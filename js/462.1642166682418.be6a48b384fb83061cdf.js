5551b9b70ed7e977110ca625633e24928470d9d7
Travis CI User <travis@example.org>
2022-01-14 21:29:05 +0000

    ImagesTable: add target labels for new image types
    The "guest-image", "image-installer", and "vsphere" image types now
    display proper labels in the ImagesTable target column. Also, the upload
    component is renamed to Target for consistency.

--- js/462.1642166682418.be6a48b384fb83061cdf.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[462],{87462:(e,r,t)=>{function n(){return n=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var t=arguments[r];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e},n.apply(this,arguments)}t.d(r,{Z:()=>n})}}]);
-//# sourceMappingURL=../sourcemaps/462.7de43ce594b1671ef5708934a6cdb775.js.map

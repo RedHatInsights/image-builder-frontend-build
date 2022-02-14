cb9675756bb6c8d10673fe88fd37f4254be79909
Travis CI User <travis@example.org>
2022-02-14 15:42:52 +0000

    CreateImageWizard: add activation keys to registration
    The registration step now provides users with a type ahead input so they
    can select from all activation keys associated with their accounts.
    
    The testing for this requires that we wait for the mocked api response.

--- js/462.1644584331504.054732da484b56b16005.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[462],{87462:(e,r,t)=>{function n(){return n=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var t=arguments[r];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e},n.apply(this,arguments)}t.d(r,{Z:()=>n})}}]);
-//# sourceMappingURL=../sourcemaps/462.0327bff39f4d16ebaa38431f2f0de7a9.js.map

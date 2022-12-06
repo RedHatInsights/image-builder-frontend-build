456ce8b4b777b6a3e83fa85a581f554f6d29e2c6
Travis CI User <travis@example.org>
2022-12-06 14:19:24 +0000

    CreateImageWizard: use non-deprecated image type aliases
    'ami' was deprecated a while ago, and is kept around as an alias of
    'aws'. Same with 'vhd' and 'azure', and 'vhd' and 'gcp'.
    
    This maintains support for the old aliases where necessary.

---
+++ js/412.1670336204308.675058c534d5a71823ef.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>o});var a=n(75418),r=n.n(a),l=n(86706),u=n(61390),c=n(38803),s=n(30319),i=n(55808);const o=function(){return r().createElement(l.zt,{store:s.h},r().createElement(u.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},r().createElement(c.Z,null)))}}}]);
+//# sourceMappingURL=../sourcemaps/412.61b11c58fb2832b04b3402964ddc6236.js.map
---
+++ js/491.1670336204308.675058c534d5a71823ef.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(75418),a=n.n(r),l=n(66235),u=n.n(l),c=n(86706),o=n(61390),m=n(38803),s=n(30319),i=n(55808);const d=function(){return a().createElement(c.zt,{store:s.h},a().createElement(o.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},a().createElement(m.Z,null)))};u().render(a().createElement(d,null),document.getElementById("root"))}}]);
+//# sourceMappingURL=../sourcemaps/491.fdf24c11092682faddb7cd2495e4103c.js.map

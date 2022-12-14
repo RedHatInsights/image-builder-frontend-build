121958b2c21cefa5959532818ecccc4b3843c498
Travis CI User <travis@example.org>
2022-12-14 09:36:49 +0000

    Wizard: Add activation key information in Registration step
    This adds a `TextList` with additional information for a chosen activation key in Registration step of the Wizard.
    
    Component `AdditionalKeyInformation` is wrapped within a new component called `RegistrationKeyInformation` for the purpose of displaying the information on the Registration step. As the component needed to use `FormSpy` element to work properly in this context within the Data Driven Forms.
    Without it the validation of the form didn't work as expected, because of a persisting reference to the element it was evaluated against.
    
    Popover for an Activation key was also updated according to SPUR mocks.

---
+++ js/412.1671010441264.93dc450ebd799c77b2ee.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>o});var a=n(75418),r=n.n(a),l=n(86706),u=n(61390),c=n(38803),s=n(30319),i=n(55808);const o=function(){return r().createElement(l.zt,{store:s.h},r().createElement(u.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},r().createElement(c.Z,null)))}}}]);
+//# sourceMappingURL=../sourcemaps/412.e68c6bbc17ea665db3b3cdb365aff36e.js.map

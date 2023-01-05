29353a3a8f5a6afa749d72233a8eafeb7fc05948
Travis CI User <travis@example.org>
2023-01-05 15:27:28 +0000

    build(deps): bump react-router-dom from 6.4.2 to 6.6.1
    Bumps [react-router-dom](https://github.com/remix-run/react-router/tree/HEAD/packages/react-router-dom) from 6.4.2 to 6.6.1.
    - [Release notes](https://github.com/remix-run/react-router/releases)
    - [Changelog](https://github.com/remix-run/react-router/blob/main/packages/react-router-dom/CHANGELOG.md)
    - [Commits](https://github.com/remix-run/react-router/commits/react-router-dom@6.6.1/packages/react-router-dom)
    
    ---
    updated-dependencies:
    - dependency-name: react-router-dom
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/412.1672923648718.6f610d4a00798bcf0f9d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>o});var a=n(75418),r=n.n(a),l=n(86706),u=n(61390),c=n(38803),s=n(30319),i=n(55808);const o=function(){return r().createElement(l.zt,{store:s.h},r().createElement(u.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},r().createElement(c.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.f25203d1fe380609c5e3a2ebdcbf7cf8.js.map

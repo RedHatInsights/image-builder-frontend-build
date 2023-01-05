a63c30a5a8fd9f60395d0759539a48b3071af03b
Travis CI User <travis@example.org>
2023-01-05 13:03:49 +0000

    build(deps-dev): bump babel-jest from 28.1.2 to 29.3.1
    Bumps [babel-jest](https://github.com/facebook/jest/tree/HEAD/packages/babel-jest) from 28.1.2 to 29.3.1.
    - [Release notes](https://github.com/facebook/jest/releases)
    - [Changelog](https://github.com/facebook/jest/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/facebook/jest/commits/v29.3.1/packages/babel-jest)
    
    ---
    updated-dependencies:
    - dependency-name: babel-jest
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

---
+++ js/491.1672923638232.d38595c610d2eedc87a7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(75418),a=n.n(r),l=n(66235),u=n.n(l),c=n(86706),o=n(61390),m=n(38803),s=n(30319),i=n(55808);const d=function(){return a().createElement(c.zt,{store:s.h},a().createElement(o.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},a().createElement(m.Z,null)))};u().render(a().createElement(d,null),document.getElementById("root"))}}]);
+//# sourceMappingURL=../sourcemaps/491.ecabec096376bd2c5ce10526ef9cb9b0.js.map

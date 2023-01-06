79b5273f55974662924df0197dca064b4322c4bd
Travis CI User <travis@example.org>
2023-01-06 09:34:15 +0000

    Wizard: Clarify variable names and comments in requestToState()
    This commit clarifies variable names and comments that were found to be
    confusing during the review of 5adc0e.

--- js/491.1672935948631.df6b93ffd1b967e53d75.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(75418),a=n.n(r),l=n(66235),u=n.n(l),c=n(86706),o=n(61390),m=n(38803),s=n(30319),i=n(55808);const d=function(){return a().createElement(c.zt,{store:s.h},a().createElement(o.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},a().createElement(m.Z,null)))};u().render(a().createElement(d,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.8e87f51c817c517e905c6299975be96d.js.map

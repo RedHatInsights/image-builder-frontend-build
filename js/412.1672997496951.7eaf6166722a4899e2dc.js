1968eca835b25a3ba3ae3cd7d79cc7e06bdd609f
Travis CI User <travis@example.org>
2023-01-06 12:31:54 +0000

    Wizard: add ouiaId to release select
    This will help QE with testing the app by having a specific locator for
    this element.

--- js/412.1672997496951.7eaf6166722a4899e2dc.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>o});var a=n(75418),r=n.n(a),l=n(86706),u=n(61390),c=n(38803),s=n(30319),i=n(55808);const o=function(){return r().createElement(l.zt,{store:s.h},r().createElement(u.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},r().createElement(c.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.bb7a287929537a0a3d6ade2e4d7ac4da.js.map

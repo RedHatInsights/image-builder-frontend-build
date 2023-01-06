37fa9f8d0d46569a088c8ebce8a8aa257f17adc4
Travis CI User <travis@example.org>
2023-01-06 13:57:11 +0000

    build(deps): bump json5 from 1.0.1 to 1.0.2
    Bumps [json5](https://github.com/json5/json5) from 1.0.1 to 1.0.2.
    - [Release notes](https://github.com/json5/json5/releases)
    - [Changelog](https://github.com/json5/json5/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/json5/json5/compare/v1.0.1...v1.0.2)
    
    ---
    updated-dependencies:
    - dependency-name: json5
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/491.1673011304838.fc0a42fbcbbc3bd88be0.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(75418),a=n.n(r),l=n(66235),u=n.n(l),c=n(86706),o=n(61390),m=n(38803),s=n(30319),i=n(55808);const d=function(){return a().createElement(c.zt,{store:s.h},a().createElement(o.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},a().createElement(m.Z,null)))};u().render(a().createElement(d,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.b32dc018502f6f61f9cf849e2aa73e59.js.map

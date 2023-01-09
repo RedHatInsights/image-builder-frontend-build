249c0175838898e88bf6e6cd0a0c45cd75c5cc1f
Travis CI User <travis@example.org>
2023-01-09 15:45:10 +0000

    build(deps-dev): bump @babel/core from 7.19.0 to 7.20.12
    Bumps [@babel/core](https://github.com/babel/babel/tree/HEAD/packages/babel-core) from 7.19.0 to 7.20.12.
    - [Release notes](https://github.com/babel/babel/releases)
    - [Changelog](https://github.com/babel/babel/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/babel/babel/commits/v7.20.12/packages/babel-core)
    
    ---
    updated-dependencies:
    - dependency-name: "@babel/core"
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/491.1673278932140.daa4909ad57cc3ee5a04.js
+++ js/491.1673278932140.daa4909ad57cc3ee5a04.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(75418),a=n.n(r),l=n(66235),u=n.n(l),c=n(86706),o=n(61390),m=n(38803),s=n(30319),i=n(55808);const d=function(){return a().createElement(c.zt,{store:s.h},a().createElement(o.BrowserRouter,{basename:(0,i.e)(window.location.pathname)},a().createElement(m.Z,null)))};u().render(a().createElement(d,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.9bbdb6c7ff9b3e7e656339445b39b373.js.map
+//# sourceMappingURL=../sourcemaps/491.54840b311d0fb333db0c77d99135f4c5.js.map

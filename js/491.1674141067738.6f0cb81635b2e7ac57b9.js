f256b42842b77ed15cc1d15c6ebb08d99d5b68c8
Travis CI User <travis@example.org>
2023-01-19 15:13:11 +0000

    Wizard: Update the File system step
    Fixes #911. Fixes #912.
    
    This updates the File system step. The changes are:
    - changed the toggle group for selecting the mode of partitioning to radio select and updated information for each radio button as mentioned in #911
    - added information to the "automatic" option of the file system configuration as mentioned in #912
    - updated information for the "manual" option of the configuration as mentioned in mocks

---
+++ js/491.1674141067738.6f0cb81635b2e7ac57b9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(93264),l=n.n(r),u=n(3644),c=n.n(u),a=n(86706),m=n(38803),s=n(30319);const i=function(){return l().createElement(a.zt,{store:s.h},l().createElement(m.Z,null))};c().render(l().createElement(i,null),document.getElementById("root"))}}]);
+//# sourceMappingURL=../sourcemaps/491.78630457fbe30918d28fb7ec6590c866.js.map

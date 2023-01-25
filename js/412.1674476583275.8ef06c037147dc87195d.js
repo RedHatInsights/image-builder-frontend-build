e5fccd9872fa8d423bfb974173b652d1981c256d
Travis CI User <travis@example.org>
2023-01-25 13:25:53 +0000

    Wizard: Update order of releases on the Image output step
    Fixes #909. Fixes #918.
    
    This updates the order of the releases on the Image output step to sort from newest to oldest. The RELEASES Object was converted to Map which remembers insertion order.
    
    The color of the `Show options for further development of RHEL` option in the Release dropdown select was also changed to blue.

--- js/412.1674476583275.8ef06c037147dc87195d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(86706),a=n(38803),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
-//# sourceMappingURL=../sourcemaps/412.1a551a8c95f0beb26e55700223eea193.js.map

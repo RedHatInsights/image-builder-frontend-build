5e445c70f4aa14d2c80dd61a072321efccb1866a
Travis CI User <travis@example.org>
2023-01-26 10:05:45 +0000

    Redux: Add RTK Query + /provisioning/v1/sources endpoint
    This commit adds the /provisioning/v1/sources endpoint via a new
    apiSlice.js which uses RTK Query's createApi method.
    
    RTK Query allows us to query the /provisioning/v1/sources endpoint using an
    automatically generated React hook, `useGetSourcesQuery`. The usage is
    something like this (from within a React component):
    `const { data: sources, isLoading, isSuccess, isError, error } =
    useGetSourcesQuery()`.
    
    This will make it much easier for us to manage data caching and just as
    importantly manage loading and error states properly.
    
    Future PRs will migrate data fetching in the other slices (clones, composes,
    repositories) to the apiSlice.
    
    For more info on RTK Query, see:
    https://redux.js.org/tutorials/essentials/part-7-rtk-query-basics

---
+++ js/412.1674727382501.dd76c79d9438f521a68c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(86706),a=n(38803),c=n(25274);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.8bbee4abc195431384341fc4c75546b6.js.map

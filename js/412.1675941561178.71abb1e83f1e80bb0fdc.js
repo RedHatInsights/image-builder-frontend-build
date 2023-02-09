266919826bc7b3c707502bd1f3d2a6abc7f0f49e
Travis CI User <travis@example.org>
2023-02-09 11:22:03 +0000

    config: remove module name in webpack config (#959)
    Due to merging the openshift and chrome plugin systems there's
    additional requirements on the format of the module name.
    
    Camelcase is the default, so the values can just be removed from the
    config.

---
+++ js/412.1675941561178.71abb1e83f1e80bb0fdc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(86706),a=n(38803),c=n(25274);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.74846e26fe4d60a81306b6d725200d94.js.map

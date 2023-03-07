3c616e59a4bb032fc67950257cb4ae65ee81899f
Travis CI User <travis@example.org>
2023-03-07 15:33:11 +0000

    ImagesTable: Only show Launch link for images using sources
    The Launch service wizard should only be used to launch AWS images that
    were created using share_with_sources (and not share_with_accounts) in
    their request.
    
    The Launch service only supports a single source at the moment, as does
    the Image Builder Frontend. Therefore, we do not pass the entire
    share_with_sources array - only the 0th element, which should be the
    `only` source for images created using the front-end. We do not expect
    full compatibility between images created using the API (which could
    theoretically have multiple sources in share_with_sources) and Image
    Builder Frontend.

---
+++ js/412.1678203030483.802758a68fe69ce8dd5b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(86706),a=n(38803),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.2f8768e0d6e6e8411219ffc3c8cbd27f.js.map

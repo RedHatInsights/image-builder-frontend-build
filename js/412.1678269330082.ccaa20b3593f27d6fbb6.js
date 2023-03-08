093d9355f41a26bb1b84211f64da51b1fea74efa
Travis CI User <travis@example.org>
2023-03-08 09:58:22 +0000

    ClonesTable: Ensure source exists before reading its account_id property
    If a clone is created using a source, and the source is then deleted,
    the source will be undefined. Attempting to read the account_id from
    undefined causes the app to crash. Optional chaining fixes this.

---
+++ js/412.1678269330082.ccaa20b3593f27d6fbb6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(86706),a=n(38803),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.fbe903a7b34d300496d7d91f25a1b439.js.map

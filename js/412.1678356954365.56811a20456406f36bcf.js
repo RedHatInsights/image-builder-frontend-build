91f7ddf114b64c5993fe756b69bd95dc1718e488
Travis CI User <travis@example.org>
2023-03-09 10:18:57 +0000

    Wizard: Ensure only one scrollbar on the Packages step
    Fixes #914.
    
    This adds a maximum height limit to the dual list selector on the Packages step.
    That way the Wizard doesn't become scrollable when the Packages search returns more results.

---
+++ js/412.1678356954365.56811a20456406f36bcf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(86706),a=n(38803),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.82550003d7932e2ea5ce2b6a69aff53b.js.map

b26aa0610db440283bc832da365f9c43f5226cbe
Travis CI User <travis@example.org>
2023-04-13 12:03:21 +0000

    Wizard: Ensure unique IDs for buttons
    Fixes #1065.
    
    This sets buttons on the Repositories and Additional custom packages steps to `CustomButtons` to ensure buttons in each of the steps have a unique ID.

--- js/412.4dfe1051f896b52b84a4.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(70743),a=n(38803),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
-//# sourceMappingURL=../sourcemaps/412.85fbb6ff177f9fca01edd8db0b9518df.js.map

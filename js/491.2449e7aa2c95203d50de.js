1731b6722893581f6f2afbaf17b8c3d6b0c34b23
Travis CI User <travis@example.org>
2023-04-14 10:17:47 +0000

    Wizard: Remove prop validation for `ReviewStepTables` and `ReviewStepTextLists`
    This removes prop validation that was left behind after the props were removed during a Review step updates PR review.

--- js/491.2449e7aa2c95203d50de.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(93264),l=n.n(r),u=n(3644),c=n.n(u),a=n(70743),m=n(38803),s=n(30319);const i=function(){return l().createElement(a.zt,{store:s.h},l().createElement(m.Z,null))};c().render(l().createElement(i,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.a6a4ec8ed72de4cdb36259f6ede55add.js.map

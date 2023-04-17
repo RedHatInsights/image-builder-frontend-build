b222768a40b322734b02174d8a793bcd3ae8f155
Travis CI User <travis@example.org>
2023-04-17 15:59:24 +0000

    build(deps): bump data driven forms to 3.20.6
    3.20.6 includes a hotfix that allows modals to take an appendTo prop,
    which is necessary for modals to display correctly with the new
    quickstart tutorials.

---
+++ js/491.33e9d38089f114055850.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(93264),l=n.n(r),u=n(3644),c=n.n(u),a=n(70743),m=n(38803),s=n(30319);const i=function(){return l().createElement(a.zt,{store:s.h},l().createElement(m.Z,null))};c().render(l().createElement(i,null),document.getElementById("root"))}}]);
+//# sourceMappingURL=../sourcemaps/491.3018b5bf48c1df6077a26197241205ea.js.map

6091c929c50473c803b29e5fc5aa0c4823a0a38f
Travis CI User <travis@example.org>
2023-04-21 10:33:31 +0000

    LaunchWizard: Add large variant prop to set width of wizard correctly
    Without this prop, the wizard's width expands to fill the entire window.

---
+++ js/412.429d9e21f26cb18e61fc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(70743),a=n(38803),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.07e10b858ed14a23da3d99c1c0445886.js.map

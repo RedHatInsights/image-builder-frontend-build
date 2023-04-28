bed09aa5a2001d0c56171d6778240c81d9e4ecc7
Travis CI User <travis@example.org>
2023-04-28 15:13:13 +0000

    LandingPage/ImagesTable/Wizard: Remove all insights global calls
    This commit replaces all calls to the insights.chrome global with calls
    to the useChrome() hook (or the useGetEnvironment() custom hook that
    wraps useChrome()).

---
+++ js/412.5f630e4405f21903e0f3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(70743),a=n(38803),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.3628c63d2fd6ce66ab94582c0de15ad6.js.map

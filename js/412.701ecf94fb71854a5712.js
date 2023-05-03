fd45e0785b98d1821e3c8e4f7f8f5c731634a5e3
Travis CI User <travis@example.org>
2023-05-03 13:31:48 +0000

    Remove embedded quickstarts & Preview
    With our quickstarts now merged to prod we can
    start using the quickstart API to invoke them.
    Also change all Beta user facing text to Preview
    
    Signed-off-by: Pavel Odvody <pavel@redhat.com>

---
+++ js/412.701ecf94fb71854a5712.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(70743),a=n(38803),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.dbe3b51e4b7de3264df8fd1bd63eb3a8.js.map

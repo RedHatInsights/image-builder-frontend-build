f8051411f0b4dfd75932a2dc3f86792bee02910c
Travis CI User <travis@example.org>
2023-05-05 10:58:14 +0000

    Wizard: Adjust AWS launch popover for images created in preview
    If an image does not contain a share_with_accounts property, launch
    links do not appear in the launch popover. This happens when a user
    creates an AWS image in beta/preview, and then clicks Launch in stable.
    
    A message is now displayed that explains the image was created using
    Preview features, and the link to Preview has been clarified to be more
    explicit.

--- js/491.ab290a5f3f500d1003ce.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(93264),l=n.n(r),u=n(3644),c=n.n(u),a=n(70743),m=n(38803),s=n(30319);const i=function(){return l().createElement(a.zt,{store:s.h},l().createElement(m.Z,null))};c().render(l().createElement(i,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.9be27bbb2779e42e4ef8521d750c5e48.js.map

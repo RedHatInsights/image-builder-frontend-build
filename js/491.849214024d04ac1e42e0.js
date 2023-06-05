b92cc0104fa0808c58c5b938ca138c1df6a1c864
Travis CI User <travis@example.org>
2023-06-05 14:25:08 +0000

    build(deps): bump @unleash/proxy-client-react from 3.5.2 to 3.6.0
    Bumps [@unleash/proxy-client-react](https://github.com/Unleash/unleash-proxy-react) from 3.5.2 to 3.6.0.
    - [Release notes](https://github.com/Unleash/unleash-proxy-react/releases)
    - [Changelog](https://github.com/Unleash/proxy-client-react/blob/main/Changelog.md)
    - [Commits](https://github.com/Unleash/unleash-proxy-react/compare/v3.5.2...v3.6.0)
    
    ---
    updated-dependencies:
    - dependency-name: "@unleash/proxy-client-react"
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/491.849214024d04ac1e42e0.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(93264),l=n.n(r),u=n(3644),c=n.n(u),a=n(70743),m=n(38803),s=n(30319);const i=function(){return l().createElement(a.zt,{store:s.h},l().createElement(m.Z,null))};c().render(l().createElement(i,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.98793610ef2096ad2fe0d4d4b8bfc572.js.map

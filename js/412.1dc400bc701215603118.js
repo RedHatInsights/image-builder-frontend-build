5f54f8da14162cbe2eb32f94754498b01adeabae
Travis CI User <travis@example.org>
2023-06-14 14:13:13 +0000

    ImagesTable: Add image details and update ClonesTable
    This adds expandable rows details for each image and updates ClonesTable according to the new mocks.

---
+++ js/412.1dc400bc701215603118.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(70743),a=n(18789),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/412.770d923653176401ecdce19972639b1e.js.map
--- js/412.8d57146a81764f236e8c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimage_builder=self.webpackChunkimage_builder||[]).push([[412],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(70743),a=n(18789),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
-//# sourceMappingURL=../sourcemaps/412.59cd5cd37575e586ef0ce654d47d1c81.js.map

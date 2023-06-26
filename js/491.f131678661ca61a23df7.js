a072456af6c091a9b50ad5042b24f848d3bc32dd
Travis CI User <travis@example.org>
2023-06-26 11:00:39 +0000

    LandingPage: Add edge images table
    This commit adds the edge images table as a federated module, gated behind a feature flag.
    the configuration is for dev environment to work with federation modules locally

--- js/491.f131678661ca61a23df7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(93264),l=n.n(r),u=n(3644),c=n.n(u),a=n(70743),m=n(18789),s=n(30319);const i=function(){return l().createElement(a.zt,{store:s.h},l().createElement(m.Z,null))};c().render(l().createElement(i,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.b32293171ba3ab23b7a1c99824370b52.js.map

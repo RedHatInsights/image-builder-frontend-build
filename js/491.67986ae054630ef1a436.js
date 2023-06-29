fbcfb5cf2b4bc5f81e57d188d4bf7c3d96e09194
Travis CI User <travis@example.org>
2023-06-29 09:19:52 +0000

    ImagesTable: Update Image commands popover for GCP images
    This adds "Launch an instance" and "Save a copy" commands to the Image commands popover for GCP images.

---
+++ js/491.67986ae054630ef1a436.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[491],{36491:(e,t,n)=>{n.r(t);var r=n(93264),l=n.n(r),u=n(3644),c=n.n(u),a=n(70743),m=n(65508),s=n(30319);const i=function(){return l().createElement(a.zt,{store:s.h},l().createElement(m.Z,null))};c().render(l().createElement(i,null),document.getElementById("root"))}}]);
+//# sourceMappingURL=../sourcemaps/491.86e97f4d96f24ce1711f5125963c1955.js.map

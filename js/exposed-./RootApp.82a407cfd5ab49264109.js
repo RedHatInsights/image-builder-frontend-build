ff2752766fa59a81219bc45d61573c3f41b72ae8
Travis CI User <travis@example.org>
2023-07-20 11:27:37 +0000

    ImagesTable: Update wording and links on empty state
    This updates text and links on empty state according to recent mocks.

---
+++ js/exposed-./RootApp.82a407cfd5ab49264109.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(70743),a=n(65508),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.10f9baebea412d605c8a0d20cfd36f46.js.map

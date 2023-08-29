1fb044d5a14db707ccd6ca5bf7619a217f681ecb
Travis CI User <travis@example.org>
2023-08-29 10:49:16 +0000

    HMS-1492: Change rhc to non-capitalized
    Fixes HMS-1492
    
    This changes all occurences of "rhc" and "remote host configuration" from capitalized to lowercase.
    In cases where the sentence started with "rhc" this was changed to "remote host configuration" to avoid
    starting the sentence with "Rhc".

--- js/exposed-./RootApp.7b010f197b2a4a799c01.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(93264),u=n.n(r),l=n(70743),a=n(65508),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
-//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.bb55c9852dc0dcf497024e245186ced8.js.map

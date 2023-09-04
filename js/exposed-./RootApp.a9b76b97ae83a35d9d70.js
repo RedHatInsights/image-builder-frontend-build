897b9e33153bba92167f48c9c6c059f0e4bf8b45
Travis CI User <travis@example.org>
2023-09-04 08:58:03 +0000

    API: Add mutations for creating composes and clones
    This commit adds mutations for the /compose and
    /composes/:composeId/clone POST end points to the Image Builder API
    slice.

---
+++ js/exposed-./RootApp.a9b76b97ae83a35d9d70.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(32747),u=n.n(r),l=n(70743),a=n(65508),c=n(30319);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.2a7b5fcfbf3ea73c10420b48a82b6679.js.map

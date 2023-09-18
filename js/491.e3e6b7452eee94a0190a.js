f065683129f56902351fe1f9f02fa65c44bb3da9
Travis CI User <travis@example.org>
2023-09-18 14:17:33 +0000

    LaunchWizard: Correct architecture prop
    The `<LaunchWizard>` architecture prop was specified incorrectly,
    which caused the Instance dropdown in the Launch wizard to fail to load
    instance types.

--- js/491.e3e6b7452eee94a0190a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[491],{36491:(e,t,r)=>{r.r(t);var n=r(32747),l=r.n(n),a=r(20745),u=r(70743),c=r(76828),o=r(56594);const s=function(){return l().createElement(u.zt,{store:o.h},l().createElement(c.Z,null))};(0,a.s)(document.getElementById("root")).render(l().createElement(s,null))},20745:(e,t,r)=>{var n=r(10056);t.s=n.createRoot,n.hydrateRoot}}]);
-//# sourceMappingURL=../sourcemaps/491.0985cb3e27ff26b6e6b30145a4e55ccb.js.map
---
+++ js/exposed-./RootApp.80dfb2bbe1ab006d259a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(32747),u=n.n(r),l=n(70743),a=n(76828),c=n(56594);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.385944a9723fd16e5d9cc17831a560ea.js.map

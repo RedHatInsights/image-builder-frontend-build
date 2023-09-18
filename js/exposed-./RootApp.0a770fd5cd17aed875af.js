88254a587479b65e8b857cfee892e212381382a1
Travis CI User <travis@example.org>
2023-09-18 11:44:20 +0000

    edge: setup an unleash toggle for the image table
    The edge images table is going to be integrated in the frontend code
    base in the coming weeks. To help smooth that development process there
    is need for a feature flag that would keep the current behavior running
    on stage and prod while the new image table is being developed.
    
    For now dummy code is returned for when the flag is on, and legacy
    behavior keeps on going when the flag is off.
    
    The stage flag can be seen there:
    - https://insights-stage.unleash.devshift.net/projects/default/features/image-builder.edge.local-image-table
    
    The prod flag can be seen there:
    - https://insights.unleash.devshift.net/projects/default/features/image-builder.edge.local-image-table
    
    The commit brings in some documentation in the Readme. Any member of
    the team should be able to follow these steps to add in new feature
    flags and use them in the code base.
    
    The commit also brings in two new testing files for the future
    components to come. For now these tests only check that the mocking
    value for the unleash flag is properly set.

--- js/exposed-./RootApp.0a770fd5cd17aed875af.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(32747),u=n.n(r),l=n(70743),a=n(65508),c=n(55506);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
-//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.1e4360fb7d8ba3c17a1df7f301d76830.js.map

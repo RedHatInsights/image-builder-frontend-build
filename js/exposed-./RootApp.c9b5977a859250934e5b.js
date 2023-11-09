280ef3ba476bf1c74d9578db0dd25e9eaa969b39
Travis CI User <travis@example.org>
2023-11-09 11:51:17 +0000

    wizard: prepare a dev env to create the new wizard
    Developing a new wizard will require many pull requests and some of them
    might even be worked on in parallel. To allow to develop all of that
    without interfering with the legacy wizard this commit introduces a new
    dev environment alongside an unleash flag when we later want to do the
    switch.
    
    To use the new dev en use `npm run stage-beta:experimental` or `npm run
    stage-beta:msw+experimental` depending on what you want.
    
    Then unleash flag to toggle in stage and prod is: `image-builder.new-wizard.enabled`
    
    fixes: HMS-2859

--- js/exposed-./RootApp.c9b5977a859250934e5b.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(32747),u=n.n(r),l=n(70743),a=n(76828),c=n(56594);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
-//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.2c9dbff4e2f6082b68a24188b73536e5.js.map

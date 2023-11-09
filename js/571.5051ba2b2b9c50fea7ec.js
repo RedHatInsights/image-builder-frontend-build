8962a43ecdb5020e6e58cdfd2f13e5db08fb905e
Travis CI User <travis@example.org>
2023-11-09 12:47:22 +0000

    package.json: pin all package versions
    Dependabot and npm ci work best together when all package versions are pinned.

--- js/571.5051ba2b2b9c50fea7ec.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[571],{96571:(e,u,r)=>{r.r(u),r.d(u,{default:()=>n});var a=r(85893);r(32747);const n=function(){return(0,a.jsx)(a.Fragment,{})}}}]);
-//# sourceMappingURL=../sourcemaps/571.54ff90d90044bbd510fefd83cd64ff1b.js.map

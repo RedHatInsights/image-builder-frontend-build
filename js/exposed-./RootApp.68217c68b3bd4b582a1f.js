258a24f6704ae3c244c9ec340371d97dfda98cea
Travis CI User <travis@example.org>
2023-11-24 14:37:41 +0000

    Wizard: Activation key management link based on env
    The "Activation keys page" link on the Registration step previously led to prod from both prod and stage envs.
    
    This fixes the problem and ensures that the button in stage links to the activation key management in stage and the prod one to prod.

---
+++ js/exposed-./RootApp.68217c68b3bd4b582a1f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(32747),u=n.n(r),l=n(70743),a=n(76828),c=n(56594);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.ac8b1e188d4c5bf0d8dd122b31b3f70c.js.map

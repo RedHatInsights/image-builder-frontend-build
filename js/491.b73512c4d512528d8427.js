2e211d3879e8b7878d2406a980f748d8f3e2644f
Travis CI User <travis@example.org>
2023-12-13 15:45:55 +0000

    wizard: remove unused code from the implementation
    There were some leftovers that needs to be removed. This commit simply
    cleans them up.

--- js/491.b73512c4d512528d8427.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[491],{36491:(e,t,r)=>{r.r(t);var n=r(32747),l=r.n(n),a=r(20745),u=r(70743),c=r(76828),o=r(56594);const s=function(){return l().createElement(u.zt,{store:o.h},l().createElement(c.Z,null))};(0,a.s)(document.getElementById("root")).render(l().createElement(s,null))},20745:(e,t,r)=>{var n=r(10056);t.s=n.createRoot,n.hydrateRoot}}]);
-//# sourceMappingURL=../sourcemaps/491.8f23f8bf823dd62ca53c0ba0c40a64a5.js.map

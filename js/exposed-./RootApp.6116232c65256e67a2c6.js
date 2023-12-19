66a2f451f569bb21f981e3b3f1bcb688d8668684
Travis CI User <travis@example.org>
2023-12-19 08:26:52 +0000

    Wizard/test: Remove unused variables
    This removes unused variable from the code.

--- js/exposed-./RootApp.6116232c65256e67a2c6.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(32747),u=n.n(r),l=n(70743),a=n(76828),c=n(56594);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
-//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.78f6833047a5cdc83f1ab15347d059f9.js.map

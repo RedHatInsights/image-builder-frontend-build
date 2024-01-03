a347454b26b2877a7684b0ea875a43db8cece1e4
Travis CI User <travis@example.org>
2024-01-03 08:01:07 +0000

    ImagesTable: fix grey box under AMI when image build is in progress
    Previously a Skeleton component was used as a placeholder for an AMI when the build was still pending.
    This removes the Skeleton and returns undefined rendering the AMI as empty.

---
+++ js/491.814523605cc5472a9046.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[491],{36491:(e,t,r)=>{r.r(t);var n=r(32747),l=r.n(n),a=r(20745),u=r(70743),c=r(76828),o=r(819);const s=function(){return l().createElement(u.zt,{store:o.h},l().createElement(c.Z,null))};(0,a.s)(document.getElementById("root")).render(l().createElement(s,null))},20745:(e,t,r)=>{var n=r(10056);t.s=n.createRoot,n.hydrateRoot}}]);
+//# sourceMappingURL=../sourcemaps/491.e328a1b07abdc28d6242a3c36cf56315.js.map

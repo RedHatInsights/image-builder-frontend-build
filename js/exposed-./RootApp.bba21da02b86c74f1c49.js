bf1020079abcb6c13afe17f7f69fa2ab68665033
Travis CI User <travis@example.org>
2024-01-18 07:49:07 +0000

    build(deps): bump @scalprum/react-core from 0.5.1 to 0.6.6
    Bumps [@scalprum/react-core](https://github.com/scalprum/scaffloding) from 0.5.1 to 0.6.6.
    - [Release notes](https://github.com/scalprum/scaffloding/releases)
    - [Changelog](https://github.com/scalprum/scaffolding/blob/main/release.config.js)
    - [Commits](https://github.com/scalprum/scaffloding/commits/@scalprum/react-core-0.6.6)
    
    ---
    updated-dependencies:
    - dependency-name: "@scalprum/react-core"
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

---
+++ js/exposed-./RootApp.bba21da02b86c74f1c49.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[8178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(32747),u=n.n(r),l=n(70743),a=n(76828),c=n(85546);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
+//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.e4e1babea24bee3e76ba0a4ecdc82283.js.map
--- js/exposed-./RootApp.fa17a185faf2409ffa0d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkimageBuilder=self.webpackChunkimageBuilder||[]).push([[8178],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r=n(32747),u=n.n(r),l=n(70743),a=n(76828),c=n(85546);const s=function(){return u().createElement(l.zt,{store:c.h},u().createElement(a.Z,null))}}}]);
-//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.2a698d4b6ecea0e0a437d0b9f18d206e.js.map
